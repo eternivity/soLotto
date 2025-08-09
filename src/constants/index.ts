a1e364496def9d7eca089cd3165869e9b9128bc5
Emrea SoLotto <emrea@solotto.com>
2025-08-10 01:21:07 +0300

    fix: Switch to Helius RPC to avoid rate limiting

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,7 +21,7 @@ export const SEASON_CONFIG = {
 };
 
 export const NETWORK_CONFIG = {
-  endpoint: "https://api.testnet.solana.com",
+  endpoint: "https://testnet.helius-rpc.com/?api-key=dfe48aac-27c8-4671-9b41-1a8f19b85b0b",
   commitment: "confirmed" as const,
 };
 
